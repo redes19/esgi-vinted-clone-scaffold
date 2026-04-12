238a6542e44395b3c0401c6b8ab27c32f960d5d3
redes19 <120016236+redes19@users.noreply.github.com>
2026-04-12 10:29:51 +0200

    start feature my articles

--- src/pages/MyArticlesPage.tsx
+++ src/pages/MyArticlesPage.tsx
@@ -1,3 +1,24 @@
+import { useCurrentUserId } from "../hooks/useCurrentUserId"
+import { api } from '../services/api'
+import { useParams } from "react-router";
+import { type Article } from '../types/article'
+import { useQuery } from "@tanstack/react-query";
+
+
 export default function MyArticlesPage() {
-  return <h1>Mes annonces</h1>;
+  const { idUser } = useCurrentUserId();
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["article", idUser],
+    queryFn: async () => const res = await api.get<Article[]>("/api/users/" + idUser + "/articles"),
+      enabled: !!idUser
+
+    });
+
+
+  return (
+    <>
+      <h1>Mes annonces</h1>
+    </>
+  );
 }
