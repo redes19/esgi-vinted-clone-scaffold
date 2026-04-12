5fd16c657c3423497b83f3a4f444d00f0fca4c87
Loris Laurenti <lloris19@outlook.fr>
2026-04-12 19:28:24 +0200

    update myarticlesPage

--- src/pages/MyArticlesPage.tsx
+++ src/pages/MyArticlesPage.tsx
@@ -1,24 +1,35 @@
 import { useCurrentUserId } from "../hooks/useCurrentUserId"
 import { api } from '../services/api'
-import { useParams } from "react-router";
 import { type Article } from '../types/article'
 import { useQuery } from "@tanstack/react-query";
 
 
 export default function MyArticlesPage() {
-  const { idUser } = useCurrentUserId();
+  const idUser = useCurrentUserId();
 
   const { data, error, isLoading } = useQuery({
     queryKey: ["article", idUser],
-    queryFn: async () => const res = await api.get<Article[]>("/api/users/" + idUser + "/articles"),
+    queryFn: async () => await api.get<Article[]>("/api/users/" + idUser + "/articles"),
       enabled: !!idUser
+  });
 
-    });
+  console.log("id : ", idUser);
 
+
+  if(!idUser) return null;
 
   return (
     <>
       <h1>Mes annonces</h1>
+
+      {error && (
+        <div>Une erreur est survenue</div>
+      )}
+
+      {isLoading && (
+        <div>Loadind...</div>
+      )}
+
     </>
   );
 }
